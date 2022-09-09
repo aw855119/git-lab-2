22131d2a1e760f075a66fc94609c7c3f0e928495
aw855119 <aw855119@ohio.edu>
2022-09-09 19:24:28 -0400

    Create git-lab-2.cc

---
+++ git-lab-2.cc
@@ -0,0 +1,18 @@
+/*
+ *        File: git-lab-program.cc
+ *      Author: <Aly Wood>
+ *        Date: <09/09/2022>
+ * Description: This code will print "Hello Git!!!" oon the user's screen.
+ */
+
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+using namespace std;
+
+
+int main(int argc, char const *argv[]) {
+
+    cout << "Hello Git!!" << endl;
+    return 0;
+}// main
